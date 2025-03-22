262d74285ca226b412431a90bb62a6f63c5227bc
Gustavo-Peixoto <gustavo_peixoto1@outlook.com>
2025-03-22 17:02:24 -0300

    aula5

---
+++ aula5/exerc2/Program.cs
@@ -0,0 +1,20 @@
+﻿Random numAleatorio = new Random();
+int valorAleatorio = numAleatorio.Next(1,100);
+int chute = 0;
+
+do
+{
+    Console.Write("Chute o valor: ");
+    chute = Convert.ToInt32(Console.ReadLine());
+    if(chute > valorAleatorio)
+    {
+        Console.WriteLine("Chutou Alto!!");
+    }
+    else if(chute < valorAleatorio)
+    {
+        Console.WriteLine("Chutou Baixo!!");
+    }
+}
+while(chute != valorAleatorio);
+
+Console.WriteLine("Você Adivinhou O Numero!!!");
---
+++ aula5/exerc5/Program.cs
@@ -0,0 +1,38 @@
+﻿int hours = 0;
+
+while(true)
+{
+    Console.Write("Insira a quantidade de horas que serão treinadas por dia: ");
+    hours = Convert.ToInt32(Console.ReadLine());
+
+    if(hours > 24)
+    {
+        Console.WriteLine("Você não Consegue treinar dois dias em um cara.");
+    }
+    else
+    {
+        break;
+    }
+}
+
+int horas_soma = 0;
+int dias = 0;
+float semanas = 0;
+int mes = 0;
+
+while(horas_soma < 1000)
+{
+    horas_soma += hours;
+    dias++;
+    if(dias == 5)
+    {
+        semanas++;
+        dias = 0;
+    }
+    if(semanas >= 4.5)
+    {
+        mes++;
+        semanas = 0;
+    }
+}
+Console.WriteLine($"meses: {mes}\nSemanas: {semanas}\nDias: {dias}");
